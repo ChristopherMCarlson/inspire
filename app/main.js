906dd5269577239095e6caacaf5957f52a41b8db
Christopher Carlson <c.m.carlson13@gmail.com>
2018-08-17 16:55:18 -0600

    end of day

--- app/main.js
+++ app/main.js
@@ -3,6 +3,26 @@ import ImageController from "./components/image/image-controller.js";
 import QuoteController from "./components/quote/quote-controller.js";
 import TodoController from "./components/todo/todo-controller.js";
 
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  h = ((h + 11) % 12 + 1);
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  let time = h + ":" + m + ":" + s
+  document.getElementById('clock').innerHTML = `
+  <div class="row clock text-center"><div class="col"><p>${time}</p></div></div>
+  <div class="row clock text-center"><div class="col"><p>Hello, user.</p></div></div>
+  `
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+  return i;
+}
+
 // HEY WHERE ARE ALL OF YOUR CONTROLLERS??
 class app {
   constructor() {
@@ -12,6 +32,7 @@ class app {
       quoteController: new QuoteController(),
       toDoController: new TodoController()
     }
+    startTime()
   }
 }
 
