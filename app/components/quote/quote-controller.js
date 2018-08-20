fa0ad38d05b2ce7e28b6d14309e28eb284430db0
Christopher Carlson <c.m.carlson13@gmail.com>
2018-08-20 16:58:39 -0600

    fixed text truncate

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -3,10 +3,18 @@ import QuoteService from "./quote-service.js";
 let qs = new QuoteService
 
 function drawQuote(res) {
-	document.getElementById('quote').innerHTML = `
-	${res[0].content}
+	let quote = res[0].content.replace(/(<p[^>]+?>|<p>|<\/p>)/img, "").split(' ');
+	if (quote.length >= 9) {
+		let newQuote = quote.slice(0, 9).join(' ')
+		document.getElementById('quote').innerHTML = `
+	<p>${newQuote}...</p>
 	<p class="author">-${res[0].title}</p>
-	`
+	`} else {
+		let newQuote = quote.join(' ')
+		document.getElementById('quote').innerHTML = `
+		<p>${newQuote}</p>
+		<p class="author">-${res[0].title}</p>
+		`}
 }
 
 export default class QuoteController {
