22d1e3602c225db922c2c691fb7166e1da009709
Christopher Carlson <c.m.carlson13@gmail.com>
2018-08-22 12:11:08 -0600

    added hover to uote

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -7,7 +7,7 @@ function drawQuote(res) {
 	if (quote.length >= 9) {
 		let newQuote = quote.slice(0, 9).join(' ')
 		document.getElementById('quote').innerHTML = `
-	<p>${newQuote}...</p>
+	<p title="${res[0].content.replace(/(<p[^>]+?>|<p>|<\/p>)/img, "")}">${newQuote}...</p>
 	<p class="author">-${res[0].title}</p>
 	`} else {
 		let newQuote = quote.join(' ')
